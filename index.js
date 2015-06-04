5d1ee1e131df17d8e29c34c9ce3755440ea8bb35
hansent <thomas.hansen@gmail.com>
2015-06-03 19:39:24 -0500

    refactored

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// private code //
+var express = require('express');
+
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static('build'));
+
+var state = [];
+
+io.on('connection', function (socket) {
+  
+  var color = "#"+((1<<24)*Math.random()|0).toString(16);
+
+  io.sockets.emit('init', { color: color, state: state });
+  
+  socket.on('addPath', function (path) {
+    var path = {color:color, from: path.from, to: path.to};
+    state.push(path);
+    io.sockets.emit('addPath', path);
+  });
+  
+
+});
+
+
+server.listen(8080);
--- public/index.js
+++ public/index.js
@@ -1,91 +1,68 @@
 var $ = require('jquery');
+var io = require('socket.io-client');
 
 var canvas = document.getElementById('canvas'),
 ctx = canvas.getContext('2d');
 
 APP = {
-    paths: [],
-    currentPath: null
+    lastPos: null
 };
+
+var socket = io();
+
+socket.on('init', function(data){
+    APP.color = data.color;
+    data.state.forEach(renderPath);
+});
+
+socket.on('addPath', function(path){
+    console.log('addPath', path);
+    renderPath(path);
+})
+
+
+var canvas = document.getElementById('canvas'),
+ctx = canvas.getContext('2d');
+
+window.addEventListener('mousedown', onMouseDown, false);
+window.addEventListener('mousemove', onMouseMove, false);
+window.addEventListener('mouseup', onMouseUp, false);
+window.addEventListener('resize', resizeCanvas, false);
+resizeCanvas();    
+
+
+
+function renderPath(path){
+    ctx.strokeStyle= path.color;
+    ctx.beginPath();
+    ctx.moveTo(path.from.x, path.from.y);    
+    ctx.lineTo(path.to.x, path.to.y);
+    ctx.stroke();    
+}
+
+
+function onMouseDown(ev){
+    APP.lastPos = {x: ev.clientX, y: ev.clientY};
+}
+
+function onMouseMove(ev){
+    var newPos = {x: ev.clientX, y: ev.clientY};
+    if (APP.lastPos != null){
+        socket.emit('addPath', {from: APP.lastPos, to: newPos});
+        APP.lastPos = newPos;
+    }
+}
+
+function onMouseUp(ev){
+    var newPos = {x: ev.clientX, y: ev.clientY};
+    if (APP.lastPos){
+        socket.emit('addPath', {from: APP.lastPos, to: newPos});
+    }
+    APP.lastPos = null;
+}
 
 
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
-
-
-function onMouseDown(ev){
-    APP.currentPath = [];
-    
-    APP.currentPath.push({x: ev.clientX, y: ev.clientY});
-    APP.currentPath.push({x: ev.clientX, y: ev.clientY});
-    APP.paths.push(APP.currentPath);
-}
-
-function onMouseMove(ev){
-    console.log("move", ev);
-    if (APP.currentPath){
-        console.log("insert point", ev);
-        APP.currentPath.push({x: ev.clientX, y: ev.clientY});   
-    }
-    
-}
-
-function onMouseUp(ev){
-    console.log(APP.currentPath);
-    APP.currentPath.push({x: ev.clientX, y: ev.clientY});
-    APP.currentPath = null;    
-}
-
-
-
-
-canvas.addEventListener('mousedown', onMouseDown, false);
-canvas.addEventListener('mousemove', onMouseMove, true);
-canvas.addEventListener('mouseup', onMouseUp, false);
-window.addEventListener('resize', resizeCanvas, false);
-
-
-function renderPath(path){
-    var p = path[0];
-    
-    for(var i=1; i<path.length; i++){
-         ctx.beginPath();
-         ctx.moveTo(p.x, p.y);    
-   
-        p = path[i];
-        ctx.lineTo(p.x, p.y);
-        ctx.stroke();    
-    }
-    
-
-    
-}
-
-function renderPaths(){
-    for (var i = 0; i <APP.paths.length; i++) {
-        renderPath(APP.paths[i]);
-    };
-}
-
-
-function render(){
-    renderPaths();
-    //window.requestAnimationFrame(render);       
-}
-
-//window.requestAnimationFrame(render);
-setInterval(render, 1000/30.0);
-
-resizeCanvas();        
-
-
-
-
-
-
-
-
-
-
